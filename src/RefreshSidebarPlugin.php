de6cc5b2eeea5682c904114119af8a580db39346
jeffersongoncalves <411493+jeffersongoncalves@users.noreply.github.com>
2025-12-23 23:58:40 +0000

    Fix styling

--- src/RefreshSidebarPlugin.php
+++ src/RefreshSidebarPlugin.php
@@ -21,11 +21,8 @@ class RefreshSidebarPlugin implements Plugin
 
     public function register(Panel $panel): void
     {
-        $panel->renderHook(PanelsRenderHook::SCRIPTS_AFTER, fn(): View => view('filament-refresh-sidebar::scripts'));
+        $panel->renderHook(PanelsRenderHook::SCRIPTS_AFTER, fn (): View => view('filament-refresh-sidebar::scripts'));
     }
 
-    public function boot(Panel $panel): void
-    {
-
-    }
+    public function boot(Panel $panel): void {}
 }
